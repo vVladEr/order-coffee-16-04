3b8abf0e2cf59ebf250f93e809aa7f2d80da20e1
DuninD <dunin-04@mail.ru>
2024-04-16 22:14:44 +0500

    all

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function createForm()
     const orders = document.getElementsByTagName('fieldset');
     const form = orders[orders.length - 1];
     const newForm = document.createElement('fieldset');
-    newForm.innerHTML = form.innerHTML;
+    let text = form.innerHTML.replace(/milk(?:\d+)?/g, `milk${count}`);
+    newForm.innerHTML = text;
     newForm.querySelector('.beverage-count').textContent = `Напиток №${++count}`;
     newForm.className = 'beverage';
     newForm.querySelector('.delete-button').addEventListener('click', (event) => deleteOrder(event));
@@ -64,15 +65,52 @@ document.querySelector('.submit-button').addEventListener('click', (event)  =>{
     head.appendChild(tr);
     document.querySelector('.lightbox').appendChild(table);
     let body = table.createTBody();
-    let orders = document.querySelectorAll('fieldset');
-    for(let order of orders)
-    {
+    let orders = document.querySelectorAll('.beverage');
+    console.log(orders);
+    orders.forEach((beverage) => {
+        let selectedMilk = beverage.querySelector(`input[type="radio"]:checked`).nextElementSibling.textContent;
+        if (selectedMilk === 'обычном молоке') {
+            selectedMilk = 'обычное молоко';
+        }
+        if (selectedMilk === 'обезжиренном молоке') {
+            selectedMilk = 'обезжиренное молоко';
+        }
+        if (selectedMilk === 'соевом молоке') {
+            selectedMilk = 'соевое молоко';
+        }
+        if (selectedMilk === 'кокосовом молоке') {
+            selectedMilk = 'кокосовое молоко';
+        }
+        let selectedOptions = [];
+        beverage.querySelectorAll(`input[type="checkbox"]:checked`).forEach((option) => {
+            let optionText = option.nextElementSibling.textContent;
+            if (optionText === 'взбитых сливок') {
+                optionText = 'взбитые сливки';
+            }
+            if (optionText === 'зефирок') {
+                optionText = 'зефирки';
+            }
+            if (optionText === 'корицу') {
+                optionText = 'корица';
+            }
+            selectedOptions.push(optionText);
+        });
         let tr = document.createElement('tr');
-        let td1 = 0;
-    }
-});
+        let td1 = document.createElement('td');
+        td1.textContent = `${beverage.querySelector('select').selectedOptions[0].text}`;
+        tr.appendChild(td1);
+        let td2 = document.createElement('td');
+        td2.textContent = `${selectedMilk}`;
+        tr.appendChild(td2);
+        let td3 = document.createElement('td');
+        td3.textContent = `${selectedOptions.join(', ')}`;
+        tr.appendChild(td3);
+        body.appendChild(tr);
+    });
+})
 document.querySelector('.overlay-close-button').addEventListener('click', (event) =>{
     event.preventDefault();
     document.querySelector('.overlay').querySelector('h3').remove();
+    document.querySelector('table').remove();
     document.querySelector('.overlay').style.display = 'none';
 });
